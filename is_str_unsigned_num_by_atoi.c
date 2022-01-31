1fb2fbe75a3fd20246c2370c696a7acc5e1be2aa
shige <shigebask@gmail.com>
2022-01-31 18:42:21 +0900

    fix:

--- is_str_unsigned_num_by_atoi.c
+++ is_str_unsigned_num_by_atoi.c
@@ -2,5 +2,5 @@
 
 bool	is_str_unsigned_num_by_atoi(int *num, char *str)
 {
-	return (is_str_int_by_atoi(num, str) == true && *num >= 0 );
+	return (is_str_int_by_atoi(num, str) == true && *num >= 0);
 }
